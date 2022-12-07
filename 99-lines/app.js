29e36db9281416076b3f215d4faca11917a4ca11
jakobwilson <115019859+jakobwilson@users.noreply.github.com>
2022-12-07 16:20:39 -0600

    Add files via upload

---
+++ 99-lines/app.js
@@ -0,0 +1,106 @@
+var friends = ['Jacob', 'Caleb', 'Logan', 'Veal', 'Tim' ];
+
+var lines;
+for (counter = 99; counter >= 1; counter = counter - 1) 
+{
+    if (counter == 1) {
+        lines = 'line';
+    } else {
+        lines = 'lines';
+    }
+    console.log(counter+" "+lines+" of code in the file.");
+    if (counter < 99) {
+        console.log("");
+        console.log(counter+" "+lines+" of code in the file.");
+    }
+    console.log(counter+" "+lines+" of code.");
+    console.log("Jacob strikes one out.");
+    console.log("clears it all out.");
+    if (counter == 1) {
+        console.log("No more lines of code in the file.");
+    }
+}
+
+var lines;
+for (counter = 99; counter >= 1; counter = counter - 1) 
+{
+    if (counter == 1) {
+        lines = 'line';
+    } else {
+        lines = 'lines';
+    }
+    console.log(counter+" "+lines+" of code in the file.");
+    if (counter < 99) {
+        console.log("");
+        console.log(counter+" "+lines+" of code in the file.");
+    }
+    console.log(counter+" "+lines+" of code.");
+    console.log("Caleb strikes one out.");
+    console.log("clears it all out.");
+    if (counter == 1) {
+        console.log("No more lines of code in the file.");
+    }
+}
+
+var lines;
+for (counter = 99; counter >= 1; counter = counter - 1) 
+{
+    if (counter == 1) {
+        lines = 'line';
+    } else {
+        lines = 'lines';
+    }
+    console.log(counter+" "+lines+" of code in the file.");
+    if (counter < 99) {
+        console.log("");
+        console.log(counter+" "+lines+" of code in the file.");
+    }
+    console.log(counter+" "+lines+" of code.");
+    console.log("Logan strikes one out.");
+    console.log("clears it all out.");
+    if (counter == 1) {
+        console.log("No more lines of code in the file.");
+    }
+}
+
+var lines;
+for (counter = 99; counter >= 1; counter = counter - 1) 
+{
+    if (counter == 1) {
+        lines = 'line';
+    } else {
+        lines = 'lines';
+    }
+    console.log(counter+" "+lines+" of code in the file.");
+    if (counter < 99) {
+        console.log("");
+        console.log(counter+" "+lines+" of code in the file.");
+    }
+    console.log(counter+" "+lines+" of code.");
+    console.log("Veal strikes one out.");
+    console.log("clears it all out.");
+    if (counter == 1) {
+        console.log("No more lines of code in the file.");
+    }
+}
+
+var lines;
+for (counter = 99; counter >= 1; counter = counter - 1) 
+{
+    if (counter == 1) {
+        lines = 'line';
+    } else {
+        lines = 'lines';
+    }
+    console.log(counter+" "+lines+" of code in the file.");
+    if (counter < 99) {
+        console.log("");
+        console.log(counter+" "+lines+" of code in the file.");
+    }
+    console.log(counter+" "+lines+" of code.");
+    console.log("Tim strikes one out.");
+    console.log("clears it all out.");
+    if (counter == 1) {
+        console.log("No more lines of code in the file.");
+    }
+}
